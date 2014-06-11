1ad031892f34d29bbf8144c759da6d621d171231
Nate Winn <nathan.winn@gmail.com>
2014-06-11 08:11:03 -0400

    added login

---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :lockable and :omniauthable
+  devise  :database_authenticatable, :registerable, :confirmable,
+  			  :recoverable, :rememberable, :trackable, :validatable, :timeoutable
+end
