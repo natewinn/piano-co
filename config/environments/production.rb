fc6e5a083d974e78d43395b96a559ed985d0385f
Nate Winn <nathan.winn@gmail.com>
2014-08-07 23:18:10 -0700

    update mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,8 +88,8 @@ Rails.application.configure do
     address:              'smtp.gmail.com',
     port:                 587,
     domain:               'gmail.com',
-    user_name:            'jow.winn',
-    password:             'jowiekid17',
+    user_name:            ENV['EMAIL'],
+    password:             ENV['PASSWORD'],
     authentication:       'plain',
     enable_starttls_auto: true  }
     
